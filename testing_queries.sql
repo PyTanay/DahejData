69352948f2e0e2b8f8ac834b84a58ca36207bf73
Tanay Desai <tanaydesai26@gmail.com>
2024-10-07 01:25:35 +0530

    Created a feature rich excel file to retrive data to excel.

--- testing_queries.sql
+++ testing_queries.sql
@@ -1,11 +1,12 @@
 SET profiling = 1;
 -- select count(*) from hourlydata;
 SELECT 
-    count(td.Description),
-    avg(hd.Value),
-    sum(td.alarmValue),
-    count(td.enggunits),
-    count(td.tagname)
+    td.Description,
+    hd.Value,
+    hd.DateTime,
+   --  sum(td.alarmValue),
+    -- count(td.enggunits),
+    td.tagname
 FROM 
     -- hourlyData hd
     partitionedHourlyData hd
@@ -13,10 +14,13 @@ JOIN
     TagDetails td ON hd.TagKey = td.TagKey
 WHERE 
     td.TagName = 'TIC36620' 
-    AND hd.DateTime BETWEEN '2015-01-01 00:00:00' AND '2025-12-31 23:59:59';
+    AND hd.DateTime BETWEEN '2015-01-01 00:00:00' AND '2020-02-15 23:59:59';
 SHOW PROFILES;
 -- OPTIMIZE TABLE partitionedHourlyData;
 -- select * from tagdetails;
 -- ALTER TABLE hourlyData ROW_FORMAT=COMPRESSED;
+-- CREATE INDEX idx_TagKey ON partitionedHourlyData (TagKey);
+-- CREATE INDEX idx_DateTime ON partitionedHourlyData (DateTime);
+-- DROP INDEX idx_DateTime ON partitionedHourlyData;
 
 
